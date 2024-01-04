186d8081e0224a9e6b9ef1b5e79ec1f5dfe9cba6
Alberto <Alberto@rebustech.io>
2024-01-04 03:17:17 -0500

    feat: edit port by railway requirement

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 python3.9 manage.py makemigrations --noinput
 python3.9 manage.py migrate --noinput
 
-gunicorn investment_manager.wsgi:application --bind 0.0.0.0:$PORT
+python3.9 manage.py runserver 0.0.0.0:$PORT
