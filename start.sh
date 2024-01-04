79454a9529bcfde7aa40b11affb5cdb7599201f6
Alberto <Alberto@rebustech.io>
2024-01-04 03:07:00 -0500

    feat: add env port to start sh

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 python3.9 manage.py makemigrations --noinput
 python3.9 manage.py migrate --noinput
 
-python3.9 manage.py runserver 0.0.0.0:8000
+gunicorn investment_manager.wsgi:application --bind 0.0.0.0:$PORT
